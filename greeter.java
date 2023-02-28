30626d1ec721a1e1f3e220f2cc68dd59fe08bb40
crt1 <christinaipsr@gmail.com>
2023-02-28 08:36:49 +0530

    initial commit

---
+++ greeter.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class greeter{
+  public static void main(String[] args){
+    System.out.println("What is your name? type your name and hit enter");
+    Scanner scanner = new Scanner(System.in);
+    String name = scanner.nextLine();
+    System.out.println("Hello " + name + "!");
+  }  
+}
